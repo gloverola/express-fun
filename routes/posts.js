b9eb92674098fefd3f81b6dc40d33d272270e673
glover <info.copdev@gmail.com>
2024-11-13 21:52:02 +0100

    feat(app): add update post

--- routes/posts.js
+++ routes/posts.js
@@ -56,4 +56,19 @@ router.post('/', (req, res) => {
   res.status(201).json(posts);
 });
 
+// Update post
+router.put('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res
+      .status(404)
+      .json({ msg: `A post with the id ${id} was not found` });
+  }
+
+  post.title = req.body.title;
+  res.status(200).json(posts);
+});
+
 export default router;
