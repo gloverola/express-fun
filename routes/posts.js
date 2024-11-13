fee0db2ecb7db237a2d0d5972111bef379765bb4
glover <info.copdev@gmail.com>
2024-11-13 21:40:12 +0100

    feat(app): add create post

---
+++ routes/posts.js
@@ -0,0 +1,59 @@
+// @ts-nocheck
+import express from 'express';
+const router = express.Router();
+
+let posts = [
+  {
+    id: 1,
+    title: 'Post 1',
+    content: 'This is post 1',
+  },
+  {
+    id: 2,
+    title: 'Post 2',
+    content: 'This is post 2',
+  },
+  {
+    id: 3,
+    title: 'Post 3',
+    content: 'This is post 3',
+  },
+];
+
+// Get all posts
+router.get('/', (req, res) => {
+  const limit = parseInt(req?.query?.limit);
+  if (!isNaN(limit)) {
+    res.status(200).json(posts.slice(0, limit));
+    return;
+  }
+  res.status(200).json(posts);
+});
+
+// Get a single post
+router.get('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (!post) {
+    res
+      .status(404)
+      .json({ message: `Post with id ${id} was not found` });
+  } else res.status(200).json(post);
+});
+
+// Create new post
+router.post('/', (req, res) => {
+  const newPost = {
+    id: posts.length + 1,
+    title: req.body.title,
+  };
+
+  if (!newPost.title) {
+    return res.status(400).json({ msg: 'Please include a title' });
+  }
+
+  posts.push(newPost);
+  res.status(201).json(posts);
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
-const { log } = require('console');
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
 const port = process.env.PORT || 8000;
 
 const app = express();
@@ -9,44 +9,12 @@ const app = express();
 // Set static folder
 // app.use(express.static(path.join(__dirname, 'public')));
 
-let posts = [
-  {
-    id: 1,
-    title: 'Post 1',
-    content: 'This is post 1',
-  },
-  {
-    id: 2,
-    title: 'Post 2',
-    content: 'This is post 2',
-  },
-  {
-    id: 3,
-    title: 'Post 3',
-    content: 'This is post 3',
-  },
-];
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// Get all posts
-app.get('/api/posts', (req, res) => {
-  const limit = parseInt(req?.query?.limit);
-  if (!isNaN(limit)) {
-    res.status(200).json(posts.slice(0, limit));
-    return;
-  }
-  res.status(200).json(posts);
-});
-
-// Get a single post
-app.get('/api/posts/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-  if (!post) {
-    res
-      .status(404)
-      .json({ message: `Post with id ${id} was not found` });
-  } else res.status(200).json(post);
-});
+// Routes
+app.use('/api/posts', posts);
 
 app.listen(port, () =>
   console.log(`Server is running on port ${port}`)
